7320a811c706e32903d85978f74fc96221d0dd97
Lee <littlecsi27@gmail.com>
2022-06-16 23:27:38 +0100

    final control esp32

--- Control/src/main.cpp
+++ Control/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <NTPClient.h>
+// #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <WiFiClientSecure.h>
 
@@ -8,17 +8,39 @@
 #include "esp_wpa2.h"
 #include <Wire.h>
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
-WiFiClientSecure client;
+#define RXP1 16 //Defining UART With Vision (Pins 8 and 9 on Arduino Adaptor)
+#define TXP1 17
+#define RXP2 18 //Defining UART With Drive (Pins 6 and 7 on Arduino Adaptor)
+#define TXP2 5
+
+#define VSPI_MISO 15 //Defining SPI with Camera on Vision (Pins 10, 11, 12 and 13 on Arduino Adaptor)
+#define VSPI_MOSI 4
+#define VSPI_SCLK 2
+#define VSPI_SS 14
 
 #define EAP_ANONYMOUS_IDENTITY "lc2120@ic.ac.uk" //anonymous identity
 #define EAP_IDENTITY "lc2120@ic.ac.uk"// user identity
 #define EAP_PASSWORD "PASSWORD" //eduroam user password
 
+WiFiUDP ntpUDP;
+// NTPClient timeClient(ntpUDP);
+WiFiClientSecure client;
+SPIClass* vspi = NULL; //Container for VSPI connection
+
+char Command[32];   // storage for the actual command
+char DriveMsg[32];  // storage for drive's message
+char VisionMsg[64]; // storage for vision's message
+
+bool drive_ready = true;        // checks whether drive is ready for receiving command
+bool drive_msg_ready = false;   // checks whether drive's message is ready for sending
+bool vision_msg_ready = false;  // checks whether vision's message is ready for sending
+bool command_ready = false;     // checks whether command is ready for sending command
+bool connected = false;         // checks whether the ESP32 has already connected with the server
+
 const char* ssid = "Imperial-WPA"; // eduroam SSID
-// const char* host = "http://192.168.203.3/"; // local
-const char* host = "http://34.226.193.83/"; // aws
+const char* host = "http://192.168.203.3/"; // local
+// const char* host = "http://34.226.193.83/"; // aws
+int port = 443;
 int counter = 0;
 const char* test_root_ca = \
                            "-----BEGIN CERTIFICATE-----\n" \
@@ -76,31 +98,181 @@ void initWiFi() {
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); // Debugging
+  Serial1.begin(115200, SERIAL_8N1, RXP1, TXP1); //Uart with Vision
+  Serial2.begin(115200, SERIAL_8N1, RXP2, TXP2); //Uart with Drive
+  
+  vspi = new SPIClass(VSPI); //Initialising VSPI connection
+  vspi->begin(VSPI_SCLK, VSPI_MISO, VSPI_MOSI, VSPI_SS);
+  vspi->setClockDivider(SPI_CLOCK_DIV8);
+
+  pinMode(VSPI_SS, OUTPUT);
 
   initWiFi();
-
-  timeClient.begin();
+  // timeClient.begin();
 }
 
 void loop() {
-  timeClient.update();
-
-  // --------------------WiFi-------------------- //
-  if (WiFi.status() == WL_CONNECTED) { // if we are connected to eduroam network
-    counter = 0; // reset counter
-    Serial.println("Wifi is still connected with IP: ");
-    Serial.println(WiFi.localIP());   // inform user about his IP address
-  } else if (WiFi.status() != WL_CONNECTED) { // if we lost connection, retry
-    WiFi.begin(ssid);
+  // timeClient.update();
+  if (!connected) {
+    if (!client.connect(host, port)) {
+      Serial.println("Connection to host failed");
+      delay(100);
+      return;
+    }
+    Serial.println("Connected to server successful!");
+    client.print("Hello from ESP32!");
+    connected = true;
   }
-  while (WiFi.status() != WL_CONNECTED) { // during lost connection, print dots
-    delay(500);
-    Serial.print(".");
-    counter++;
-    if (counter >= 60) { // 30 seconds timeout - reset board
-      ESP.restart();
+
+  // Checks if there is any data on the UART Vision datastream
+  if (Serial1.available()) {
+    // read the bytes incoming from the UART Port:
+    for (int i = 0; i < 64; i++) {
+      VisionMsg[i] = ' ';
+    }
+    char Visioninit = Serial1.read();
+
+    if (!vision_msg_ready) {
+      VisionMsg[0] = '[';
+      VisionMsg[1] = Visioninit;
+      int i = 2;
+
+      while (Serial1.available()) {
+        char Visionchar = Serial1.read();
+
+        if (Visionchar != '\n') {
+          VisionMsg[i] = Visionchar;
+          i++;
+        } else {
+          Serial.println("The message from Vision has been recorded.");
+          VisionMsg[i] = ']';
+          break;
+        }
+      }
     }
   }
-  Serial.println(WiFi.localIP());
+
+  // Checks if there is any data on the UART Drive datastream
+  if (Serial2.available()) {
+    // read the bytes incoming from the UART Port:
+    char Driveinint = Serial2.read();
+
+    if (Driveinint == '@' && !drive_ready) {
+      Serial.println("Drive is ready to receive a command");
+      drive_ready = true;
+      vision_msg_ready = true;
+    } else if (Driveinint == 'D' && !drive_msg_ready) {
+      int i = 0;
+
+      while (Serial2.available()) {
+        char Drivechar = Serial2.read();
+
+        if (Drivechar != '@' || Drivechar != 'Q') {
+          DriveMsg[i] = Drivechar;
+          i++;
+        } else {
+          Serial.println("The message from Drive has been recorded.");
+          drive_msg_ready = true;
+          break;
+        }
+      }
+    } else if (Driveinint == 'Q' && !drive_msg_ready) {
+      int i = 0;
+      
+      while (Serial2.available()) {
+        char Drivechar = Serial2.read();
+        
+        if (Drivechar != '@') {
+          DriveMsg[i] = Drivechar;
+          i++;
+        } else {
+          Serial.println("The message from Drive has been recorded.");
+          drive_msg_ready = true;
+          break;
+        }
+      }
+    }
+  }
+
+  // Checks if there is any data on the TCP datastream and 
+  // if so, reads it and echos it back to the server.
+  if (client.available() && !command_ready) {
+    // read the bytes incoming from the server:
+    char Commandinit = client.read();
+
+    if (Commandinit == '[') {
+      Command[0] = Commandinit;
+      int i = 1;
+
+      while (client.available()) {
+        char Commandchar = client.read();
+
+        if (Commandchar != ']') {
+          Command[i]] = Commandchar;
+          i++;
+        } else {
+          Command[i] = ']';
+          Serial.println("The Command has been recorded.");
+          command_ready = true;
+          break;
+        }
+      }
+    } else if (Commandinit == 'S') {
+      Command[0] = Commandinit;
+
+      Serial.println("The Stop signal has been recorded.");
+      Serial.print("Sending Stop signal to drive: ");
+
+      Serial.write(Command[0]);
+      Serial2.write(Command[0]);
+
+      Command[0] = ' ';
+    }
+  }
+
+  // Checks if drive and command are ready for moving the rover
+  if (drive_ready && command_ready) {
+    Serial.print("Sending command to drive: ");
+
+    for (int i = 0; i < 32; i++) {
+      Serial.write(Command[i]);
+      Serial2.write(Command[i]);
+      Command[i] = ' ';
+    }
+    Serial.println();
+    Serial2.write('\n');
+
+    drive_ready = false;
+    command_ready = false;
+  }
+
+  // Checks if drive has a message for command
+  if (drive_msg_ready) {
+    Serial.print("Sending message to command: ");
+
+    for (int i = 0; i < 32; i++) {
+      Serial.write(DriveMsg[i]);
+      client.write(DriveMsg[i]);
+
+      DriveMsg[i] = ' ';
+    }
+    Serial.println();
+    client.write('\n');
+    drive_msg_ready = false;
+  }
+
+  // Checks if vision has a message for command
+  if (vision_msg_ready) {
+    Serial.print("Sending message to command: ");
+
+    for (int i = 0; i < 64; i++) {
+      Serial.write(VisionMsg[i]);
+      client.write(VisionMsg[i]);
+
+      VisionMsg[i] = ' ';
+    }
+    Serial.println();
+    vision_msg_ready = false;
+  }
 }
